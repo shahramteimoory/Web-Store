89b0e9030daa1ddc83e784638fd4336724b73dff
shahramteimoory <Darn.shan@yahoo.com>
2023-09-23 18:30:36 +0330

    First Commit

---
+++ EndPoint.site/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EndPoint.site.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
