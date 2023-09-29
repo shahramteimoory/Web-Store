ea12f763dbade94c3f5f7ee7316e316d971a1f09
shahramteimoory <Darn.shan@yahoo.com>
2023-09-29 11:37:02 +0330

    Add Entity Category

---
+++ Web-Store.Domain/Entites/Products/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web_Store.Domain.Entites.Products
+{
+    public class Category
+    {
+        public string Name { get; set; }
+
+        public virtual Category ParentCategory { get; set; }
+        public long? ParentCategoryId { get; set; }
+
+
+        public virtual ICollection<Category> SubCategories { get; set; }
+    }
+}
