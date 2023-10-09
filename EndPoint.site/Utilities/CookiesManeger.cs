756973c61586ddc545629fe589471702550a8524
shahramteimoory <Darn.shan@yahoo.com>
2023-10-09 16:58:54 +0330

    Cookies

---
+++ EndPoint.site/Utilities/CookiesManeger.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+
+namespace EndPoint.site.Utilities
+{
+    public class CookiesManeger
+    {
+        public void Add(HttpContext context, string token, string value)
+        {
+            context.Response.Cookies.Append(token, value, getCookieOptions(context));
+        }
+
+        public bool Contains(HttpContext context, string token)
+        {
+            return context.Request.Cookies.ContainsKey(token);
+        }
+
+        public string GetValue(HttpContext context, string token)
+        {
+            string cookieValue;
+            if (!context.Request.Cookies.TryGetValue(token, out cookieValue))
+            {
+                return null;
+            }
+            return cookieValue;
+        }
+
+        public void Remove(HttpContext context, string token)
+        {
+            if (context.Request.Cookies.ContainsKey(token))
+            {
+                context.Response.Cookies.Delete(token);
+            }
+        }
+
+
+        private CookieOptions getCookieOptions(HttpContext context)
+        {
+            return new CookieOptions
+            {
+                HttpOnly = true,
+                Path = context.Request.PathBase.HasValue ? context.Request.PathBase.ToString() : "/",
+                Secure = context.Request.IsHttps,
+                Expires = DateTime.Now.AddDays(100),
+            };
+        }
+    }
+}
+
